d668c786ad4026604253b8926c8902b2e789a64b
wy04313 <wy04313@126.com>
2021-12-29 10:50:26 +0800

    bak

--- js/total.js
+++ js/total.js
@@ -299,9 +299,9 @@
                     center : ['22%', '55%'],    // 默认全局居中
                     radius : '90%',
                     min:0,
-                    max:10000,
+                    max:500000,
                     endAngle:55,
-                    splitNumber:4,
+                    splitNumber:5,
                     axisLine: {            // 坐标轴线
                         lineStyle: {       // 属性lineStyle控制线条样式
                             color: [[0.29, 'lime'],[0.86, '#1e90ff'],[1, '#ff4500']],
